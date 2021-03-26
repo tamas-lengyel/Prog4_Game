dede9a03e44e901a4ce8b68bcf4c17416b34abcd
Ács Péter <parasztragu@gmail.com>
2021-03-26 11:58:47 +0100

    Main project created

---
+++ oenik_prog4_2021_1_eg6bpp_jatfmq/oenik_prog4_2021_1_eg6bpp_jatfmq/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace oenik_prog4_2021_1_eg6bpp_jatfmq
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
