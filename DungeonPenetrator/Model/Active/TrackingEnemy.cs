a9e239c3115518f754e6e05316a6def6b3f03d0a
tamas-lengyel <lengyeltamas97@gmail.com>
2021-04-08 16:48:39 +0200

    Model fixes #2

--- DungeonPenetrator/Model/Active/TrackingEnemy.cs
+++ DungeonPenetrator/Model/Active/TrackingEnemy.cs
@@ -12,5 +12,7 @@ namespace Model.Active
         public override Point Cords { get; set; }
         public override int Health { get; set; }
         public override int Damage { get; set; }
+
+        public EventHandler MoveToPlayer;
     }
 }
