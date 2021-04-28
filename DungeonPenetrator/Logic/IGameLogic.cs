1b74347929407b12058f1eae3d8303e548d28e92
tamas-lengyel <lengyeltamas97@gmail.com>
2021-04-28 16:01:16 +0200

    Started stylecop on Logic

--- DungeonPenetrator/Logic/IGameLogic.cs
+++ DungeonPenetrator/Logic/IGameLogic.cs
@@ -1,34 +1,51 @@
-﻿using Model;
-using Model.Active;
-using Model.Passive;
-using System;
-using System.Collections.Generic;
-using System.Windows;
+﻿// <copyright file="IGameLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Logic
 {
+    using System.Windows;
+    using Model;
+    using Model.Active;
+    using Model.Passive;
+
+    /// <summary>
+    /// Interface for GameLogic.
+    /// </summary>
     public interface IGameLogic
     {
         // Playerwise
         void MovePlayer(int dx, int dy);
+
         void PlayerShoot(Point mousePos, int speed);
 
         // Enemywise
         void MoveFlyingEnemy(FlyingEnemy flyingEnemy);
+
         void MoveRegularEnemy(ActiveGameObjects activeGameObjects);
+
         Projectile EnemyShoot(Point enemyLocation, int speed, int damage);
+
         void DisposeEnemy(ActiveGameObjects activeGameObject);
+
         void UpdatePlayerInSight();
+
         void BossPatternShoot(Point bossLocation, int speed, int damage);
+
         void RandomBossMovement(Point bossLocation);
+
         Projectile BossShoot(Point bossLocation, int speed, int damage);
 
         void Updater();
-        void MoveProjectile(ref Projectile projectile);
-        void CollectPowerup(Powerups powerups);
-        void DamageActiveGameObject(ActiveGameObjects activeGameObjects, int damage);
-        void DisposeBullet(Projectile projectile);
-        void DropRandomCollectable();
 
+        void MoveProjectile(ref Projectile projectile);
+
+        void CollectPowerup(Powerups powerups);
+
+        void DamageActiveGameObject(ActiveGameObjects activeGameObjects, int damage);
+
+        void DisposeBullet(Projectile projectile);
+
+        void DropRandomCollectable();
     }
 }
--- DungeonPenetrator/Logic/ILoadingLogic.cs
+++ DungeonPenetrator/Logic/ILoadingLogic.cs
@@ -1,19 +1,25 @@
-﻿using Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="ILoadingLogic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Logic
 {
+    using System.Linq;
+    using Model;
+
+    /// <summary>
+    /// Interface for LoadingLogic.
+    /// </summary>
     public interface ILoadingLogic
     {
         void EndGame(string name);
-        void GenerateMap();
-        IQueryable<Highscore> GetHighscores();
-        void NextLevel();
-        IGameModel Play();
 
+        void GenerateMap();
+
+        IQueryable<Highscore> GetHighscores();
+
+        void NextLevel();
+
+        IGameModel Play();
     }
 }
