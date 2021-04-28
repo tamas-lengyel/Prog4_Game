13198bdbb94cc5228ca7127fa3fa6d630135961f
Ács Péter <parasztragu@gmail.com>
2021-04-28 17:17:56 +0200

    Documentation added for the interfaces

--- DungeonPenetrator/Logic/ILoadingLogic.cs
+++ DungeonPenetrator/Logic/ILoadingLogic.cs
@@ -12,14 +12,32 @@ namespace Logic
     /// </summary>
     public interface ILoadingLogic
     {
+        /// <summary>
+        /// Defines the game state changes when the gameplay is over.
+        /// </summary>
+        /// <param name="name">Defines the name for saving purposes.</param>
         void EndGame(string name);
 
+        /// <summary>
+        /// Generates a map whether it's moving between levels or if it's the first generation.
+        /// </summary>
         void GenerateMap();
 
+        /// <summary>
+        /// Gets all the saved highscores.
+        /// </summary>
+        /// <returns>A pre defined highscore entity.</returns>
         IQueryable<Highscore> GetHighscores();
 
+        /// <summary>
+        /// Updates the game state to the next level.
+        /// </summary>
         void NextLevel();
 
+        /// <summary>
+        /// Loads in a gamestate either from a saved or newly generated one.
+        /// </summary>
+        /// <returns>The game state.</returns>
         IGameModel Play();
     }
 }
