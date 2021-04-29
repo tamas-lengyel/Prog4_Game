7c473bca03208170189fe489173bfc472047a764
tamas-lengyel <lengyeltamas97@gmail.com>
2021-04-29 12:00:16 +0200

    Warning fixes

--- DungeonPenetrator/Logic/GameLogic.cs
+++ DungeonPenetrator/Logic/GameLogic.cs
@@ -2,6 +2,8 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
+[assembly: System.CLSCompliant(false)]
+
 namespace Logic
 {
     using System;
--- DungeonPenetrator/LogicTest/GlobalSuppressions.cs
+++ DungeonPenetrator/LogicTest/GlobalSuppressions.cs
@@ -1,8 +1,7 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Performance", "CA1814:Prefer jagged arrays over multidimensional", Justification = "<Pending>", Scope = "member", Target = "~M:LogicTest.Testing.Init")]
+[assembly: SuppressMessage("Performance", "CA1814:Prefer jagged arrays over multidimensional", Justification = "Nor relevant.", Scope = "member", Target = "~M:LogicTest.Testing.Init")]
